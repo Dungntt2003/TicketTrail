b10e6a193a8f38d8675818ba1cb008f973711c4a
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-12-07 22:19:41 +0700

    Fix size in login and register ui

--- frontend/src/login/login.c
+++ frontend/src/login/login.c
@@ -129,8 +129,9 @@ void create_login_widget(int socket) {
     sock = socket;
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Login");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    // gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    // gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_fullscreen(GTK_WINDOW(window));
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     GtkWidget *overlay = gtk_overlay_new();
--- frontend/src/register/register.c
+++ frontend/src/register/register.c
@@ -166,8 +166,9 @@ void create_register_widget(int socket){
     sock = socket;
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Register");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 600);
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    // gtk_window_set_default_size(GTK_WINDOW(window), 400, 600);
+    // gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_fullscreen(GTK_WINDOW(window));
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
 
