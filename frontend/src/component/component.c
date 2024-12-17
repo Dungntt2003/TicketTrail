5a62c2cc0bbfb179d2cdbfb96c211ebb0ee71d9c
mchau98 <nguynminhchau98@gmail.com>
2024-12-17 20:33:21 +0700

    add notification in header

--- frontend/src/component/component.c
+++ frontend/src/component/component.c
@@ -32,34 +32,34 @@ static void on_button_toggled(GtkToggleButton *button, gpointer user_data) {
 }
 
 GtkWidget* create_header(GtkWidget **buttons) {
-    GtkWidget *header, *logo, *menu_box, *home_button, *ticket_button, *account_button;
+    GtkWidget *header, *logo, *menu_box, *home_button, *ticket_button, *account_button, *notification_button;
 
-    
+    // Tạo box chứa header
     header = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     gtk_widget_set_size_request(header, 1060, 40);
 
-    
     gtk_widget_set_name(header, "header");
     gtk_widget_set_margin_top(header, 10);
-    gtk_widget_set_margin_start(header, 20); 
-    gtk_widget_set_margin_end(header, 280); 
+    gtk_widget_set_margin_start(header, 20);
+    gtk_widget_set_margin_end(header, 280);
     gtk_widget_set_margin_bottom(header, 10);
 
-    
+    // Logo
     logo = gtk_image_new_from_file("../assets/images/logo.png");
     gtk_box_pack_start(GTK_BOX(header), logo, FALSE, FALSE, 10);
-    gtk_widget_set_margin_end(logo, 250); 
+    gtk_widget_set_margin_end(logo, 250);
 
-    
+    // Menu box
     menu_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     gtk_box_set_homogeneous(GTK_BOX(menu_box), TRUE);
 
-    
+    // Tạo các nút
     home_button = gtk_toggle_button_new_with_label("Home");
     ticket_button = gtk_toggle_button_new_with_label("Ticket");
     account_button = gtk_toggle_button_new_with_label("Account");
+    notification_button = gtk_toggle_button_new_with_label("Notification");
 
-    
+    // Áp dụng CSS cho các nút
     GtkCssProvider *button_provider = gtk_css_provider_new();
     gtk_css_provider_load_from_data(button_provider,
         "button {"
@@ -86,43 +86,46 @@ GtkWidget* create_header(GtkWidget **buttons) {
         "  color: white;"
         "}"
         "button:checked {"
-        "  background: #223A60;" 
+        "  background: #223A60;"
         "  color: white;"
-        
         "}",
         -1, NULL);
 
-    
+    // Thêm CSS cho từng nút
     gtk_style_context_add_provider(gtk_widget_get_style_context(home_button), GTK_STYLE_PROVIDER(button_provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
     gtk_style_context_add_provider(gtk_widget_get_style_context(ticket_button), GTK_STYLE_PROVIDER(button_provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
     gtk_style_context_add_provider(gtk_widget_get_style_context(account_button), GTK_STYLE_PROVIDER(button_provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+    gtk_style_context_add_provider(gtk_widget_get_style_context(notification_button), GTK_STYLE_PROVIDER(button_provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-    
+    // Cài đặt kích thước nút
     gtk_widget_set_size_request(home_button, 100, 40);
     gtk_widget_set_size_request(ticket_button, 100, 40);
     gtk_widget_set_size_request(account_button, 100, 40);
+    gtk_widget_set_size_request(notification_button, 130, 40);
     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(home_button), TRUE);
 
-    
+    // Thêm các nút vào menu box
     gtk_box_pack_start(GTK_BOX(menu_box), home_button, TRUE, TRUE, 10);
     gtk_box_pack_start(GTK_BOX(menu_box), ticket_button, TRUE, TRUE, 10);
     gtk_box_pack_start(GTK_BOX(menu_box), account_button, TRUE, TRUE, 10);
+    gtk_box_pack_start(GTK_BOX(menu_box), notification_button, TRUE, TRUE, 10);
 
-    
+    // Thêm menu box vào header
     gtk_box_pack_start(GTK_BOX(header), menu_box, TRUE, TRUE, 0);
 
-    
+    // Lưu các nút vào mảng
     buttons[0] = home_button;
     buttons[1] = ticket_button;
     buttons[2] = account_button;
-    buttons[3] = NULL;  
+    buttons[3] = notification_button;
+    buttons[4] = NULL;
 
-    
+    // Kết nối tín hiệu
     g_signal_connect(home_button, "toggled", G_CALLBACK(on_button_toggled), buttons);
     g_signal_connect(ticket_button, "toggled", G_CALLBACK(on_button_toggled), buttons);
     g_signal_connect(account_button, "toggled", G_CALLBACK(on_button_toggled), buttons);
+    g_signal_connect(notification_button, "toggled", G_CALLBACK(on_button_toggled), buttons);
     g_signal_connect(home_button, "clicked", G_CALLBACK(on_home_button_clicked), NULL);
 
     return header;
 }
-
